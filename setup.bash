55fa430f867f386b77cb675b0fc800c92c97122d
offerm <offerm@exchange-install-test-machine.c.bustling-opus-194302.internal>
2018-10-27 14:14:14 +0000

    First commit

---
+++ setup.bash
@@ -0,0 +1,13 @@
+alias lndbtc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10002'
+alias lndltc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10001'
+alias btcctl='btcctl --simnet --rpcuser=xu --rpcpass=xu '
+alias ltcctl='ltcctl --simnet --rpcuser=xu --rpcpass=xu --rpcserver=localhost:19556 '
+alias lndbtc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10002'
+alias lndltc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10001'
+alias xucli='xucli -c ~/xud-simnet/xud-wd/tls.cert'
+
+export GOPATH=~/xud-simnet/go
+
+export PATH=~/xud-simnet/xud/bin:$GOPATH/bin:~/xud-simnet/scripts:$PATH
+
+
