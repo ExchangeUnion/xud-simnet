ba7bd62dce74821c4101e1e42a35919c16f5657a
Offer Markovich <offerm@PayKey.com>
2018-11-01 10:47:18 +0200

    cleanup

--- setup.bash
+++ setup.bash
@@ -2,8 +2,6 @@ alias lndbtc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:
 alias lndltc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10001'
 alias btcctl='btcctl --simnet --rpcuser=xu --rpcpass=xu '
 alias ltcctl='ltcctl --simnet --rpcuser=xu --rpcpass=xu --rpcserver=localhost:19556 '
-alias lndbtc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10002'
-alias lndltc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10001'
 alias xucli='xucli -c ~/xud-simnet/xud-wd/tls.cert'
 
 export GOPATH=~/xud-simnet/go
@@ -11,11 +9,11 @@ export GOPATH=~/xud-simnet/go
 export PATH=~/xud-simnet/xud/bin:$GOPATH/bin:~/xud-simnet/scripts:$PATH
 
 case "$OSTYPE" in
-  solaris*) echo "SOLARIS" ;;
-  darwin*)  echo "OSX" ;alias tac='tail -r';;
-  linux*)   echo "LINUX" ;;
-  bsd*)     echo "BSD" ;;
-  msys*)    echo "WINDOWS" ;;
-  *)        echo "unknown: $OSTYPE" ;;
+  solaris*) echo "SOLARIS is not yet supported" ;;
+  darwin*)  alias tac='tail -r';;
+  linux*)   ;;
+  bsd*)     echo "BSD is not yet supported" ;;
+  msys*)    echo "WINDOWS is not yet supported" ;;
+  *)        echo "$OSTYPE is not yet supported" ;;
 esac
 
