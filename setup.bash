40586e7d41be75c5b71c6873288f7be24e94d156
Offer Markovich <offerm@PayKey.com>
2018-11-01 21:47:22 +0200

    cleanup

--- setup.bash
+++ setup.bash
@@ -1,3 +1,32 @@
+
+function is_running {
+if pgrep -f  "$1" > /dev/null
+then
+    return 1
+else
+    return 0
+fi
+}
+
+function confirm_not_running {
+if ! is_running "$2"
+then
+    echo "$1 is running. Ensure all processes stopped before you continue. You may use xud-simnet-stop."
+    exit 1
+else
+    return 0
+fi
+}
+
+function confirm_all_not_running {
+confirm_not_running "btcd" "btcd.*18555"
+confirm_not_running "ltcd" "ltcd.*19555"
+confirm_not_running "lndbtc" "lnd.*10012"
+confirm_not_running "lndltc" "lnd.*10011"
+confirm_not_running "xud" "node.*10002.*10001"
+}
+
+
 alias lndbtc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10002'
 alias lndltc-lncli='lncli --network simnet --no-macaroons --rpcserver=localhost:10001'
 alias btcctl='btcctl --simnet --rpcuser=xu --rpcpass=xu '
